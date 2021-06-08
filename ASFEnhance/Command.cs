8582d9b37e3e9f82372420a1d6dca99085d49958
Chr_ <chr@chrxw.com>
2021-06-08 15:00:07 +0800

    删除Point命令，该功能已集成到ASF本体

--- ASFEnhance/Command.cs
+++ ASFEnhance/Command.cs
@@ -27,18 +27,8 @@ namespace ASFEnhance
                 case "KEY":
                     return ResponseGrubKeys(message);
 
-                case "P" when args.Length > 1:
-                case "POINT" when args.Length > 1:
-                    ASF.ArchiLogger.LogGenericInfo("多个bot");
-                    return await ResponseGetPointSummary(steamID, Utilities.GetArgsAsText(args, 1, ",")).ConfigureAwait(false);
-                case "P":
-                case "POINT":
-                    ASF.ArchiLogger.LogGenericInfo("单个bot");
-                    return await ResponseGetPointSummary(bot, steamID).ConfigureAwait(false);
-
                 case "PA":
-                    return await ResponseGetPointSummary(steamID, "ASF").ConfigureAwait(false);
-
+                    return await bot.Commands.Response(steamID, "POINTS ASF").ConfigureAwait(false);
 
                 case "LA":
                     return await bot.Commands.Response(steamID, "LEVEL ASF").ConfigureAwait(false);
@@ -61,57 +51,6 @@ namespace ASFEnhance
             string rs = GrubKeys.String2keysString(message);
             return !string.IsNullOrEmpty(rs) ? rs : "未找到结果";
         }
-
-
-        // 获取账号点数余额
-        private static async Task<string?> ResponseGetPointSummary(Bot bot, ulong steamID)
-        {
-            if (steamID == 0)
-            {
-                throw new ArgumentOutOfRangeException(nameof(steamID));
-            }
-
-            if (!bot.IsConnectedAndLoggedOn)
-            {
-                return FormatBotResponse(bot, Strings.BotNotConnected);
-            }
-
-            string? message = await WebRequest.GetPointSummary(bot).ConfigureAwait(false);
-
-            return FormatBotResponse(bot, message ?? Properties.Resources.BotPointFailed);
-        }
-
-        // 获取账号点数余额(多个bot)
-        private static async Task<string?> ResponseGetPointSummary(ulong steamID, string botNames)
-        {
-            ASF.ArchiLogger.LogGenericInfo(botNames);
-
-            if (steamID == 0)
-            {
-                throw new ArgumentOutOfRangeException(nameof(steamID));
-            }
-
-            if (string.IsNullOrEmpty(botNames))
-            {
-                throw new ArgumentNullException(nameof(botNames));
-            }
-
-            HashSet<Bot>? bots = Bot.GetBots(botNames);
-
-            if ((bots == null) || (bots.Count == 0))
-            {
-                return ASF.IsOwner(steamID) ? FormatStaticResponse(string.Format(CultureInfo.CurrentCulture, Strings.BotNotFound, botNames)) : null;
-            }
-
-            IList<string?> results = await Utilities.InParallel(bots.Select(bot => ResponseGetPointSummary(bot, steamID))).ConfigureAwait(false);
-
-            List<string> responses = new(results.Where(result => !string.IsNullOrEmpty(result))!);
-
-            return responses.Count > 0 ? string.Join(Environment.NewLine, responses) : null;
-        }
-
-        internal static string FormatStaticResponse(string response) => Commands.FormatStaticResponse(response);
-        internal static string FormatBotResponse(Bot bot, string response) => bot.Commands.FormatBotResponse(response);
 
         public void OnLoaded()
         {
