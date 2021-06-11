4d61a40c340d31bf9301e2004cf378fc95e154a9
Chr_ <chr@chrxw.com>
2021-06-11 09:43:50 +0800

    增加添加愿望单命令 增加移除愿望单命令 增加添加免费游戏短命令

--- ASFEnhance/Command.cs
+++ ASFEnhance/Command.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Composition;
 using System.Globalization;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using ArchiSteamFarm;
 using ArchiSteamFarm.Json;
@@ -36,14 +37,180 @@ namespace ASFEnhance
                 case "BA":
                     return await bot.Commands.Response(steamID, "BALANCE ASF").ConfigureAwait(false);
 
-                //case "ADD" when args.Length > 1:
-                //    return await bot.Commands.Response(steamID, "ADDLICENSE" + string.Join(",", args)).ConfigureAwait(false);
+                case "AL" when args.Length > 1:
+                    return await bot.Commands.Response(steamID, "ADDLICENSE " + Utilities.GetArgsAsText(message, 1)).ConfigureAwait(false);
 
+                case "ADDWISHLIST" when args.Length > 2:
+                case "AW" when args.Length > 2:
+                    return await ResponseAddWishlist(steamID, args[1], Utilities.GetArgsAsText(message, 2)).ConfigureAwait(false);
+                case "ADDWISHLIST" when args.Length > 1:
+                case "AW" when args.Length > 1:
+                    return await ResponseAddWishlist(bot, steamID, args[1]).ConfigureAwait(false);
+
+                case "REMOVEWISHLIST" when args.Length > 2:
+                case "RW" when args.Length > 2:
+                    return await ResponseRemoveWishlist(steamID, args[1], Utilities.GetArgsAsText(message, 2)).ConfigureAwait(false);
+                case "REMOVEWISHLIST" when args.Length > 1:
+                case "RW" when args.Length > 1:
+                    return await ResponseRemoveWishlist(bot, steamID, args[1]).ConfigureAwait(false);
 
                 default:
                     return null;
             }
         }
+
+
+        // 添加愿望单
+        private static async Task<string?> ResponseAddWishlist(Bot bot, ulong steamID, string targetGameIDs)
+        {
+            if ((steamID == 0) || !new SteamID(steamID).IsIndividualAccount)
+            {
+                throw new ArgumentOutOfRangeException(nameof(steamID));
+            }
+
+            if (string.IsNullOrEmpty(targetGameIDs))
+            {
+                throw new ArgumentNullException(nameof(targetGameIDs));
+            }
+
+            if (!bot.HasAccess(steamID, BotConfig.EAccess.Master))
+            {
+                return null;
+            }
+
+            if (!bot.IsConnectedAndLoggedOn)
+            {
+                return FormatBotResponse(bot, Strings.BotNotConnected);
+            }
+
+            StringBuilder response = new();
+
+            string[] games = targetGameIDs.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string game in games)
+            {
+                if (!uint.TryParse(game, out uint gameID) || (gameID == 0))
+                {
+                    response.AppendLine(FormatBotResponse(bot, string.Format(CultureInfo.CurrentCulture, Strings.ErrorIsInvalid, nameof(gameID))));
+                    continue;
+                }
+
+                bool result = await WebRequest.AddWishlist(bot, gameID).ConfigureAwait(false);
+
+                response.AppendLine(FormatBotResponse(bot, string.Format(CultureInfo.CurrentCulture, Strings.BotAddLicense, gameID, result ? EResult.OK : EResult.Fail)));
+
+            }
+
+            return response.Length > 0 ? response.ToString() : null;
+        }
+
+        // 添加愿望单(多个bot)
+        private static async Task<string?> ResponseAddWishlist(ulong steamID, string botNames, string targetGameIDs)
+        {
+            if ((steamID == 0) || !new SteamID(steamID).IsIndividualAccount)
+            {
+                throw new ArgumentOutOfRangeException(nameof(steamID));
+            }
+
+            if (string.IsNullOrEmpty(botNames))
+            {
+                throw new ArgumentNullException(nameof(botNames));
+            }
+
+            if (string.IsNullOrEmpty(targetGameIDs))
+            {
+                throw new ArgumentNullException(nameof(targetGameIDs));
+            }
+
+            HashSet<Bot>? bots = Bot.GetBots(botNames);
+
+            if ((bots == null) || (bots.Count == 0))
+            {
+                return ASF.IsOwner(steamID) ? FormatStaticResponse(string.Format(CultureInfo.CurrentCulture, Strings.BotNotFound, botNames)) : null;
+            }
+
+            IList<string?> results = await Utilities.InParallel(bots.Select(bot => ResponseAddWishlist(bot, steamID, targetGameIDs))).ConfigureAwait(false);
+
+            List<string> responses = new(results.Where(result => !string.IsNullOrEmpty(result))!);
+
+            return responses.Count > 0 ? string.Join(Environment.NewLine, responses) : null;
+        }
+
+        // 移除愿望单
+        private static async Task<string?> ResponseRemoveWishlist(Bot bot, ulong steamID, string targetGameIDs)
+        {
+            if ((steamID == 0) || !new SteamID(steamID).IsIndividualAccount)
+            {
+                throw new ArgumentOutOfRangeException(nameof(steamID));
+            }
+
+            if (string.IsNullOrEmpty(targetGameIDs))
+            {
+                throw new ArgumentNullException(nameof(targetGameIDs));
+            }
+
+            if (!bot.HasAccess(steamID, BotConfig.EAccess.Master))
+            {
+                return null;
+            }
+
+            if (!bot.IsConnectedAndLoggedOn)
+            {
+                return FormatBotResponse(bot, Strings.BotNotConnected);
+            }
+
+            StringBuilder response = new();
+
+            string[] games = targetGameIDs.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string game in games)
+            {
+                if (!uint.TryParse(game, out uint gameID) || (gameID == 0))
+                {
+                    response.AppendLine(FormatBotResponse(bot, string.Format(CultureInfo.CurrentCulture, Strings.ErrorIsInvalid, nameof(gameID))));
+                    continue;
+                }
+
+                bool result = await WebRequest.RemoveWishlist(bot, gameID).ConfigureAwait(false);
+
+                response.AppendLine(FormatBotResponse(bot, string.Format(CultureInfo.CurrentCulture, Strings.BotAddLicense, gameID, result ? EResult.OK : EResult.Fail)));
+            }
+
+            return response.Length > 0 ? response.ToString() : null;
+        }
+
+        // 移除愿望单(多个bot)
+        private static async Task<string?> ResponseRemoveWishlist(ulong steamID, string botNames, string targetGameIDs)
+        {
+            if ((steamID == 0) || !new SteamID(steamID).IsIndividualAccount)
+            {
+                throw new ArgumentOutOfRangeException(nameof(steamID));
+            }
+
+            if (string.IsNullOrEmpty(botNames))
+            {
+                throw new ArgumentNullException(nameof(botNames));
+            }
+
+            if (string.IsNullOrEmpty(targetGameIDs))
+            {
+                throw new ArgumentNullException(nameof(targetGameIDs));
+            }
+
+            HashSet<Bot>? bots = Bot.GetBots(botNames);
+
+            if ((bots == null) || (bots.Count == 0))
+            {
+                return ASF.IsOwner(steamID) ? FormatStaticResponse(string.Format(CultureInfo.CurrentCulture, Strings.BotNotFound, botNames)) : null;
+            }
+
+            IList<string?> results = await Utilities.InParallel(bots.Select(bot => ResponseRemoveWishlist(bot, steamID, targetGameIDs))).ConfigureAwait(false);
+
+            List<string> responses = new(results.Where(result => !string.IsNullOrEmpty(result))!);
+
+            return responses.Count > 0 ? string.Join(Environment.NewLine, responses) : null;
+        }
+
 
         // 提取KEY
         private static string? ResponseGrubKeys(string message)
@@ -54,8 +221,18 @@ namespace ASFEnhance
 
         public void OnLoaded()
         {
-            ASF.ArchiLogger.LogGenericInfo("欢迎使用 ASFEnhance By Chr_ , 联系方式 chr@@chrxw.com");
+            Version version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+
+            ASF.ArchiLogger.LogGenericInfo(string.Format("欢迎使用 ASFEnhance {0}.{1}.{2}", version.Major, version.Minor, version.Build));
+            ASF.ArchiLogger.LogGenericInfo("作者 Chr_, 联系方式 chr@chrxw.com");
         }
+
+
+        internal static string FormatStaticResponse(string response) => Commands.FormatStaticResponse(response);
+        internal static string FormatBotResponse(Bot bot, string response) => bot.Commands.FormatBotResponse(response);
+
+
+
     }
 
 }
---
+++ ASFEnhance/WebRequest.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArchiSteamFarm;
+using AngleSharp.Dom;
+using System.IO;
+using static ArchiSteamFarm.Json.Steam;
+using static ArchiSteamFarm.WebBrowser;
+using SteamKit2;
+using ArchiSteamFarm.Localization;
+
+namespace ASFEnhance
+{
+    internal static class WebRequest
+    {
+        internal static async Task<bool> AddWishlist(Bot bot, uint gameID)
+        {
+            if (gameID == 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(gameID));
+            }
+
+            const string request = "/api/addtowishlist";
+            string referer = string.Format("{0}/app/{1}", SteamStoreURL, gameID);
+
+            string? sessionID = bot.ArchiWebHandler.WebBrowser.CookieContainer.GetCookieValue(SteamStoreURL, "sessionid");
+
+            if (string.IsNullOrEmpty(sessionID))
+            {
+                bot.ArchiLogger.LogNullError(nameof(sessionID));
+                return false;
+            }
+
+            Dictionary<string, string> data = new(2, StringComparer.Ordinal)
+            {
+                { "appid", gameID.ToString() },
+                { "sessionid", sessionID! }
+            };
+
+            ObjectResponse<EResultResponse>? response = await bot.ArchiWebHandler.UrlPostToJsonObjectWithSession<EResultResponse>(SteamStoreURL, request, data: data, referer: referer).ConfigureAwait(false);
+
+            if (response == null)
+            {
+                return false;
+            }
+
+            if (response.Content.Result != EResult.OK)
+            {
+                bot.ArchiLogger.LogGenericWarning(Strings.WarningFailed);
+
+                return false;
+            }
+
+            return true;
+        }
+        internal static async Task<bool> RemoveWishlist(Bot bot, uint gameID)
+        {
+            if (gameID == 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(gameID));
+            }
+
+            const string request = "/api/removefromwishlist";
+            string referer = string.Format("{0}/app/{1}", SteamStoreURL, gameID);
+
+            string? sessionID = bot.ArchiWebHandler.WebBrowser.CookieContainer.GetCookieValue(SteamStoreURL, "sessionid");
+
+            if (string.IsNullOrEmpty(sessionID))
+            {
+                bot.ArchiLogger.LogNullError(nameof(sessionID));
+                return false;
+            }
+
+            Dictionary<string, string> data = new(2, StringComparer.Ordinal)
+            {
+                { "appid", gameID.ToString() },
+                { "sessionid", sessionID! }
+            };
+
+            ObjectResponse<EResultResponse>? response = await bot.ArchiWebHandler.UrlPostToJsonObjectWithSession<EResultResponse>(SteamStoreURL, request, data: data, referer: referer).ConfigureAwait(false);
+
+            if (response == null)
+            {
+                return false;
+            }
+
+            if (response.Content.Result != EResult.OK)
+            {
+                bot.ArchiLogger.LogGenericWarning(Strings.WarningFailed);
+
+                return false;
+            }
+
+            return true;
+        }
+
+        internal static string SteamStoreURL => ArchiWebHandler.SteamStoreURL;
+    }
+}
