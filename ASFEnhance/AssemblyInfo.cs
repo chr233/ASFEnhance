74d19d11f5fb4667011dc7e9322ada97d3156dc1
Chr_ <chr@chrxw.com>
2021-07-27 11:36:37 +0800

    update

--- ASFEnhance/AssemblyInfo.cs
+++ ASFEnhance/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.5.3.45")]
-[assembly: AssemblyFileVersion("1.5.3.45")]
+[assembly: AssemblyVersion("1.5.3.49")]
+[assembly: AssemblyFileVersion("1.5.3.49")]
