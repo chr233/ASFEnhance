a52a2bb9dcfc263e502841ad50513291993ef6a1
Chr_ <chr@chrxw.com>
2021-07-27 11:35:48 +0800

    修改资源文件命名

--- ASFEnhance/Localization/Localization.zh-CN.Designer.cs
+++ ASFEnhance/Localization/Localization.zh-CN.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ASFEnhance.Properties {
+namespace ASFEnhance.Localization {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace ASFEnhance.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ASFEnhance.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ASFEnhance.Localization.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
