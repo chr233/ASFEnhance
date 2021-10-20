2b651194067d365859bc5971cec174fd6261a7c5
Chr_ <chr@chrxw.com>
2021-10-20 13:05:08 +0800

    删除无用 using

--- ASFEnhance/Profile/WebRequest.cs
+++ ASFEnhance/Profile/WebRequest.cs
@@ -1,13 +1,10 @@
 ﻿#pragma warning disable CS8632 // 只能在 "#nullable" 注释上下文内的代码中使用可为 null 的引用类型的注释。
 
-using ArchiSteamFarm.Core;
 using ArchiSteamFarm.Steam;
 using ArchiSteamFarm.Steam.Integration;
 using ArchiSteamFarm.Web.Responses;
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using static Chrxw.ASFEnhance.Cart.Response;
 
 namespace Chrxw.ASFEnhance.Profile
 
