f0284965c41c82382172e503287fd2fdc3945f62
Chr_ <chr@chrxw.com>
2021-10-25 19:31:20 +0800

    update

--- ASFEnhance/AssemblyInfo.cs
+++ ASFEnhance/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 
 [assembly: System.CLSCompliant(false)]
-[assembly: AssemblyVersion("1.5.7.187")]
-[assembly: AssemblyFileVersion("1.5.7.187")]
+[assembly: AssemblyVersion("1.5.7.192")]
+[assembly: AssemblyFileVersion("1.5.7.192")]
 
 [assembly: AssemblyCopyright("Copyright © 2021 Chr_")]
 [assembly: AssemblyProduct("ASFEnhance")]
