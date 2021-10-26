07851291add7a6df0850f72cd76ae841a1641ab6
Chr_ <chr@chrxw.com>
2021-10-26 11:38:35 +0900

    修改引用ASF的路径

--- ASFEnhance/AssemblyInfo.cs
+++ ASFEnhance/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 
 [assembly: System.CLSCompliant(false)]
-[assembly: AssemblyVersion("1.5.7.193")]
-[assembly: AssemblyFileVersion("1.5.7.193")]
+[assembly: AssemblyVersion("1.5.7.194")]
+[assembly: AssemblyFileVersion("1.5.7.194")]
 
 [assembly: AssemblyCopyright("Copyright © 2021 Chr_")]
 [assembly: AssemblyProduct("ASFEnhance")]
