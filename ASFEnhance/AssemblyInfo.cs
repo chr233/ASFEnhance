303a11f186d19388248b1f86573a7bfbe345d250
Chr_ <chr@chrxw.com>
2022-05-29 21:43:36 +0800

    修改csproj

--- ASFEnhance/AssemblyInfo.cs
+++ ASFEnhance/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 
 [assembly: System.CLSCompliant(false)]
-[assembly: AssemblyVersion("1.6.2.457")]
-[assembly: AssemblyFileVersion("1.6.2.457")]
+[assembly: AssemblyVersion("1.6.2.459")]
+[assembly: AssemblyFileVersion("1.6.2.459")]
 
 [assembly: AssemblyCopyright("Copyright © 2022 Chr_")]
 [assembly: AssemblyProduct("ASFEnhance")]
