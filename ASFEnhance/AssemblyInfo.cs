d7c0b73ec40ec5cd7fb63979725e821445834b15
Chr_ <chr@chrxw.com>
2022-09-08 22:09:15 +0800

    style 修改版本号

--- ASFEnhance/AssemblyInfo.cs
+++ ASFEnhance/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 
 [assembly: System.CLSCompliant(false)]
-[assembly: AssemblyVersion("1.6.10.666")]
-[assembly: AssemblyFileVersion("1.6.10.666")]
+[assembly: AssemblyVersion("1.6.10.667")]
+[assembly: AssemblyFileVersion("1.6.10.667")]
 
 [assembly: AssemblyCopyright("Copyright © 2022 Chr_")]
 [assembly: AssemblyProduct("ASFEnhance")]
