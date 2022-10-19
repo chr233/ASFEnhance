a5ce01f1e254081db77504920a1e099dfbd89e06
chr_ <chr@chrxw.com>
2022-10-19 15:31:15 +0800

    feat 新增`EVENT`命令

--- ASFEnhance/AssemblyInfo.cs
+++ ASFEnhance/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 
 [assembly: System.CLSCompliant(false)]
-[assembly: AssemblyVersion("1.6.15.0")]
-[assembly: AssemblyFileVersion("1.6.15.0")]
+[assembly: AssemblyVersion("1.6.16.0")]
+[assembly: AssemblyFileVersion("1.6.16.0")]
 
 [assembly: AssemblyCopyright("Copyright © 2022 Chr_")]
 [assembly: AssemblyProduct("ASFEnhance")]
