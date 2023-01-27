4f1f0b36610216a0c2fec1a10dc3789ff5c25b5b
Chr_ <chr@chrxw.com>
2023-01-27 18:08:32 +0800

    feat 新增 `UNLOCKPOINTBADGE` 命令

--- ASFEnhance/Event/WebRequest.cs
+++ ASFEnhance/Event/WebRequest.cs
@@ -1,7 +1,7 @@
 ﻿using AngleSharp.Dom;
 using ArchiSteamFarm.Steam;
 using ArchiSteamFarm.Steam.Integration;
-using ArchiSteamFarm.Web.Responses;
+using ASFEnhance.Localization;
 using System.Text.RegularExpressions;
 using static ASFEnhance.Utils;
 
@@ -53,6 +53,35 @@ namespace ASFEnhance.Event
             var match = MatchClanaCCountId().Match(community);
 
             return match.Success ? match.Groups[1].Value : null;
+        }
+
+        /// <summary>
+        /// 兑换点数徽章
+        /// </summary>
+        /// <param name="bot"></param>
+        /// <param name="defId"></param>
+        /// <param name="level"></param>
+        /// <returns></returns>
+        internal static async Task<string> RedeemPointsForBadgeLevel(Bot bot, uint defId, uint level)
+        {
+            (_, string? token) = await bot.ArchiWebHandler.CachedAccessToken.GetValue().ConfigureAwait(false);
+
+            if (string.IsNullOrEmpty(token))
+            {
+                return Langs.NetworkError;
+            }
+
+            Uri request = new(SteamApiURL, "/ILoyaltyRewardsService/RedeemPointsForBadgeLevel/v1/");
+
+            Dictionary<string, string> data = new(3) {
+                { "access_token", token },
+                { "defid", defId.ToString() },
+                { "num_levels", level.ToString() },
+            };
+
+            var response = await bot.ArchiWebHandler.UrlPostToHtmlDocumentWithSession(request, data: data, session: ArchiWebHandler.ESession.None).ConfigureAwait(false);
+
+            return response?.StatusCode == System.Net.HttpStatusCode.OK ? Langs.Done : Langs.Failure;
         }
     }
 }
