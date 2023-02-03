f6592177a46e91682cf3b4bb0a6408a5496beed3
chr_ <chr@chrxw.com>
2023-02-03 21:45:01 +0800

    misc 修改版本

--- ASFEnhance/AssemblyInfo.cs
+++ ASFEnhance/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
 [assembly: CLSCompliant(false)]
-[assembly: AssemblyVersion("1.7.12.1")]
+[assembly: AssemblyVersion("1.8.0.0")]
 [assembly: AssemblyCopyright("Copyright © 2022 Chr_")]
 [assembly: AssemblyProduct("ASFEnhance")]
 [assembly: AssemblyCompany("chrxw.com")]
