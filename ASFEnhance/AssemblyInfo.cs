87784828ec5797904ca13ef62ada4527b5b21f49
Chr_ <chr@chrxw.com>
2023-02-05 14:16:40 +0800

    misc 修改文件结构

--- ASFEnhance/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: CLSCompliant(false)]
-[assembly: AssemblyVersion("1.7.13.0")]
-[assembly: AssemblyCopyright("Copyright © 2022 Chr_")]
-[assembly: AssemblyProduct("ASFEnhance")]
-[assembly: AssemblyCompany("chrxw.com")]
-[assembly: AssemblyTitle("An ArchiSteamFarm plugin developed by Chr_.")]
-[assembly: AssemblyDescription("An ArchiSteamFarm plugin developed by Chr_.")]
