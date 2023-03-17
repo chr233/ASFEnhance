10286d0a23ab156f37cbee44c045fd7517eb023a
chr_ <chr@chrxw.com>
2023-03-17 09:39:10 +0800

    feat Addback `EVENT` command

--- ASFEnhance/Event/WebRequest.cs
+++ ASFEnhance/Event/WebRequest.cs
@@ -1,5 +1,6 @@
 ﻿using AngleSharp.Dom;
 using ArchiSteamFarm.Steam;
+using ArchiSteamFarm.Steam.Integration;
 using System.Text.RegularExpressions;
 
 namespace ASFEnhance.Event
@@ -51,5 +52,46 @@ namespace ASFEnhance.Event
 
             return match.Success ? match.Groups[1].Value : null;
         }
+
+        /// <summary>
+        /// 获取Token
+        /// </summary>
+        /// <param name="bot"></param>
+        /// <returns></returns>
+        internal static async Task<string?> FetchToken(Bot bot)
+        {
+            Uri request = new(SteamStoreURL, "/category/sports");
+
+            var response = await bot.ArchiWebHandler.UrlGetToHtmlDocumentWithSession(request, referer: request).ConfigureAwait(false);
+
+            if (response == null)
+            {
+                return null;
+            }
+
+            var configEle = response?.Content?.QuerySelector<IElement>("#application_config");
+            string community = configEle?.GetAttribute("data-loyalty_webapi_token") ?? "";
+            var match = MatchToken().Match(community);
+
+            return match.Success ? match.Groups[1].Value : null;
+        }
+
+        /// <summary>
+        /// 领取冬促贴纸
+        /// </summary>
+        /// <param name="bot"></param>
+        /// <returns></returns>
+        internal static async Task<bool> ClaimDailySticker(Bot bot, string token)
+        {
+            Uri request = new($"https://api.steampowered.com/ISaleItemRewardsService/ClaimItem/v1?access_token={token}");
+            Uri referer = new(SteamStoreURL, "/category/sports");
+
+            await bot.ArchiWebHandler.UrlPostWithSession(request, referer: referer, session: ArchiWebHandler.ESession.None).ConfigureAwait(false);
+
+            return true;
+        }
+
+        [GeneratedRegex("\"(.+)\"")]
+        private static partial Regex MatchToken();
     }
 }
