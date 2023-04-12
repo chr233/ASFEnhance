0d9d4deb901e849242744bf31669e305e2341308
Chr_ <chr@chrxw.com>
2023-04-12 21:45:08 +0800

    misc steamId转换方式修改

--- ASFEnhance/Utils.cs
+++ ASFEnhance/Utils.cs
@@ -77,7 +77,7 @@ namespace ASFEnhance
         /// <returns></returns>
         internal static ulong SteamId2Steam32(ulong steamId)
         {
-            return steamId - 0x110000100000000;
+            return steamId & 0x001111011111111;
         }
 
         /// <summary>
@@ -87,7 +87,7 @@ namespace ASFEnhance
         /// <returns></returns>
         internal static ulong Steam322SteamId(ulong steamId)
         {
-            return steamId + 0x110000100000000;
+            return steamId | 0x110000100000000;
         }
 
         /// <summary>
