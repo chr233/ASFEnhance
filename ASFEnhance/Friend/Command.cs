1f54ce9e4ce3d5aca5c187c5f9a314ebf0103c08
chr_ <chr@chrxw.com>
2023-04-17 21:39:41 +0800

    feat 新增 删除好友功能

--- ASFEnhance/Friend/Command.cs
+++ ASFEnhance/Friend/Command.cs
@@ -28,14 +28,12 @@ namespace ASFEnhance.Friend
                 return FormatStaticResponse(string.Format(Strings.BotNotFound, query));
             }
 
-            var results = await Utilities.InParallel(targetBots.Select(targetBot => WebRequest.SendFriendRequest(bot, targetBot.SteamID))).ConfigureAwait(false);
-
             StringBuilder sb = new();
-            sb.AppendLine(bot.FormatBotResponse(Langs.MultipleLineResult));
-
-            foreach (var (steamId, response) in results)
+            foreach (var targetBot in targetBots)
             {
-                sb.Append(string.Format(Langs.SendBotFriendRequest, steamId, response?.Result == EResult.OK ? Langs.Success : Langs.Failure));
+                bot.SteamFriends.AddFriend(targetBot.SteamID);
+                sb.Append(string.Format(Langs.SendBotFriendRequest, targetBot.BotName, Langs.Success));
+                await Task.Delay(200).ConfigureAwait(false);
             }
 
             return sb.ToString();
@@ -112,8 +110,8 @@ namespace ASFEnhance.Friend
 
                 if (steamId != null)
                 {
-                    var (_, response) = await WebRequest.SendFriendRequest(bot, steamId.Value).ConfigureAwait(false);
-                    sb.AppendLine(string.Format(Langs.CookieItem, entry, response?.Result == EResult.OK ? "发送好友请求成功" : "发送好友请求失败"));
+                    bot.SteamFriends.AddFriend(steamId);
+                    sb.AppendLine(string.Format(Langs.CookieItem, entry, "发送好友请求成功"));
                 }
                 else
                 {
@@ -145,6 +143,146 @@ namespace ASFEnhance.Friend
             }
 
             IList<string?> results = await Utilities.InParallel(bots.Select(bot => ResponseAddFriend(bot, query))).ConfigureAwait(false);
+
+            List<string> responses = new(results.Where(result => !string.IsNullOrEmpty(result))!);
+
+            return responses.Count > 0 ? string.Join(Environment.NewLine, responses) : null;
+        }
+
+        /// <summary>
+        /// 删除好友
+        /// </summary>
+        /// <param name="bot"></param>
+        /// <returns></returns>
+        internal static async Task<string?> ResponseDeleteFriend(Bot bot, string query)
+        {
+            if (!bot.IsConnectedAndLoggedOn)
+            {
+                return bot.FormatBotResponse(Strings.BotNotConnected);
+            }
+
+            StringBuilder sb = new();
+
+            string[] entries = query.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (entries.Length > 1)
+            {
+                sb.AppendLine(Langs.MultipleLineResult);
+            }
+
+            foreach (string entry in entries)
+            {
+                ulong? steamId;
+
+                if (ulong.TryParse(entry, out ulong value))
+                {
+                    steamId = await WebRequest.GetSteamIdByProfileLink(bot, $"/profiles/{entry}").ConfigureAwait(false) ??
+                        await WebRequest.GetSteamIdByProfileLink(bot, $"/id/{entry}").ConfigureAwait(false);
+
+                    // 好友代码
+                    if (steamId == null && value < 0xFFFFFFFF)
+                    {
+                        value = SteamId2Steam32(value);
+                        steamId = await WebRequest.GetSteamIdByProfileLink(bot, $"/profiles/{value}").ConfigureAwait(false);
+                    }
+                }
+                else
+                {
+                    steamId = await WebRequest.GetSteamIdByProfileLink(bot, $"/id/{entry}").ConfigureAwait(false);
+                }
+
+                if (steamId != null)
+                {
+                    bot.SteamFriends.RemoveFriend(steamId);
+                    sb.AppendLine(string.Format(Langs.CookieItem, entry, "发送好友请求成功"));
+                    await Task.Delay(200).ConfigureAwait(false);
+                }
+                else
+                {
+                    sb.AppendLine(string.Format(Langs.CookieItem, entry, "未找到个人资料"));
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// 删除好友 (多个Bot)
+        /// </summary>
+        /// <param name="botNames"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        internal static async Task<string?> ResponseDeleteFriend(string botNames, string query)
+        {
+            if (string.IsNullOrEmpty(botNames))
+            {
+                throw new ArgumentNullException(nameof(botNames));
+            }
+
+            HashSet<Bot>? bots = Bot.GetBots(botNames);
+
+            if ((bots == null) || (bots.Count == 0))
+            {
+                return FormatStaticResponse(string.Format(Strings.BotNotFound, botNames));
+            }
+
+            IList<string?> results = await Utilities.InParallel(bots.Select(bot => ResponseDeleteFriend(bot, query))).ConfigureAwait(false);
+
+            List<string> responses = new(results.Where(result => !string.IsNullOrEmpty(result))!);
+
+            return responses.Count > 0 ? string.Join(Environment.NewLine, responses) : null;
+        }
+
+        /// <summary>
+        /// 删除所有好友
+        /// </summary>
+        /// <param name="bot"></param>
+        /// <returns></returns>
+        internal static async Task<string?> ResponseDeleteAllFriend(Bot bot)
+        {
+            if (!bot.IsConnectedAndLoggedOn)
+            {
+                return bot.FormatBotResponse(Strings.BotNotConnected);
+            }
+
+            int friendCount = bot.SteamFriends.GetFriendCount();
+            if (friendCount > 0)
+            {
+                for (int i = 0; i < friendCount; i++)
+                {
+                    var steamId = bot.SteamFriends.GetFriendByIndex(i);
+                    bot.SteamFriends.RemoveFriend(steamId);
+                    await Task.Delay(500).ConfigureAwait(false);
+                }
+                return bot.FormatBotResponse(string.Format("成功删除了 {0} 个好友", friendCount));
+            }
+            else
+            {
+                return bot.FormatBotResponse("没有好友可以删除");
+            }
+        }
+
+        /// <summary>
+        /// 删除所有好友 (多个Bot)
+        /// </summary>
+        /// <param name="botNames"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        internal static async Task<string?> ResponseDeleteAllFriend(string botNames)
+        {
+            if (string.IsNullOrEmpty(botNames))
+            {
+                throw new ArgumentNullException(nameof(botNames));
+            }
+
+            HashSet<Bot>? bots = Bot.GetBots(botNames);
+
+            if ((bots == null) || (bots.Count == 0))
+            {
+                return FormatStaticResponse(string.Format(Strings.BotNotFound, botNames));
+            }
+
+            IList<string?> results = await Utilities.InParallel(bots.Select(bot => ResponseDeleteAllFriend(bot))).ConfigureAwait(false);
 
             List<string> responses = new(results.Where(result => !string.IsNullOrEmpty(result))!);
 
--- ASFEnhance/Friend/WebRequest.cs
+++ ASFEnhance/Friend/WebRequest.cs
@@ -7,25 +7,6 @@ namespace ASFEnhance.Friend
     internal static class WebRequest
     {
         /// <summary>
-        /// 添加好友
-        /// </summary>
-        /// <param name="bot"></param>
-        /// <param name="steamId"></param>
-        /// <returns></returns>
-        internal static async Task<(ulong, AjaxAddFriendResponse?)> SendFriendRequest(Bot bot, ulong steamId)
-        {
-            Uri request = new(SteamCommunityURL, $"/actions/AddFriendAjax");
-
-            Dictionary<string, string> data = new(3) {
-                {"steamid", steamId.ToString()},
-                {"accept_invite", "0"},
-            };
-
-            var response = await bot.ArchiWebHandler.UrlPostToJsonObjectWithSession<AjaxAddFriendResponse>(request, data: data, referer: SteamCommunityURL, session: ArchiWebHandler.ESession.CamelCase).ConfigureAwait(false);
-            return (steamId, response?.Content);
-        }
-
-        /// <summary>
         /// 验证个人资料链接
         /// </summary>
         /// <param name="bot"></param>
