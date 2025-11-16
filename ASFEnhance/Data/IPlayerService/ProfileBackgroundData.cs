d78bb6d99281587756c183fe918c64599093dd07
chr_ <chr@chrxw.com>
2025-11-16 16:49:38 +0800

    misc ASF -> 6.3.0.1

--- ASFEnhance/Data/IPlayerService/ProfileBackgroundData.cs
+++ ASFEnhance/Data/IPlayerService/ProfileBackgroundData.cs
@@ -1,41 +1,80 @@
 using System.Text.Json.Serialization;
 
 namespace ASFEnhance.Data.IPlayerService;
+/// <summary>
+/// 
+/// </summary>
 public sealed record ProfileBackgroundData
 {
+    /// <summary>
+    /// 
+    /// </summary>
     [JsonPropertyName("communityitemid")]
     public string? CommunityItemId { get; set; }
 
+    /// <summary>
+    /// 
+    /// </summary>
     [JsonPropertyName("image_large")]
     public string? ImageLarge { get; set; }
 
+    /// <summary>
+    /// 
+    /// </summary>
     [JsonPropertyName("name")]
     public string? Name { get; set; }
 
+    /// <summary>
+    /// 
+    /// </summary>
     [JsonPropertyName("item_title")]
     public string? ItemTitle { get; set; }
 
+    /// <summary>
+    /// 
+    /// </summary>
     [JsonPropertyName("item_description")]
     public string? ItemDescription { get; set; }
 
+    /// <summary>
+    /// 
+    /// </summary>
     [JsonPropertyName("appid")]
     public uint AppId { get; set; }
 
+    /// <summary>
+    /// 
+    /// </summary>
     [JsonPropertyName("item_type")]
     public int ItemType { get; set; }
 
+    /// <summary>
+    /// 
+    /// </summary>
     [JsonPropertyName("item_class")]
     public int ItemClass { get; set; }
 
+    /// <summary>
+    /// 
+    /// </summary>
     [JsonPropertyName("movie_webm")]
     public string? MovieWebm { get; set; }
 
+    /// <summary>
+    /// 
+    /// </summary>
     [JsonPropertyName("movie_mp4")]
     public string? MovieMp4 { get; set; }
 
+    /// <summary>
+    /// 
+    /// </summary>
     [JsonPropertyName("movie_webm_small")]
     public string? MovieWebmSmall { get; set; }
 
+    /// <summary>
+    /// 
+    /// </summary>
     [JsonPropertyName("movie_mp4_small")]
     public string? MovieMp4Small { get; set; }
 }
--- ASFEnhance/Data/IPlayerService/ProfileModifierItemData.cs
+++ ASFEnhance/Data/IPlayerService/ProfileModifierItemData.cs
@@ -2,32 +2,62 @@ using System.Text.Json.Serialization;
 
 namespace ASFEnhance.Data.IPlayerService;
 
+/// <summary>
+/// 
+/// </summary>
 public sealed record ProfileModifierItemData
 {
+    /// <summary>
+    /// 
+    /// </summary>
     [JsonPropertyName("communityitemid")]
     public string? CommunityItemId { get; init; }
 
+    /// <summary>
+    /// 
+    /// </summary>
     [JsonPropertyName("image_small")]
     public string? ImageSmall { get; init; }
 
+    /// <summary>
+    /// 
+    /// </summary>
     [JsonPropertyName("image_large")]
     public string? ImageLarge { get; init; }
 
+    /// <summary>
+    /// 
+    /// </summary>
     [JsonPropertyName("name")]
     public string? Name { get; init; }
 
+    /// <summary>
+    /// 
+    /// </summary>
     [JsonPropertyName("item_title")]
     public string? ItemTitle { get; init; }
 
+    /// <summary>
+    /// 
+    /// </summary>
     [JsonPropertyName("item_description")]
     public string? ItemDescription { get; init; }
 
+    /// <summary>
+    /// 
+    /// </summary>
     [JsonPropertyName("appid")]
     public uint AppId { get; init; }
 
+    /// <summary>
+    /// 
+    /// </summary>
     [JsonPropertyName("item_type")]
     public int ItemType { get; init; }
 
+    /// <summary>
+    /// 
+    /// </summary>
     [JsonPropertyName("item_class")]
     public int ItemClass { get; init; }
 }
