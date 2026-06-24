62e68bef84d2bf6eea4f5ae6d847980ebf9ef105
Chr_ <chr@chrxw.com>
2026-06-24 16:25:36 +0800

    misc

--- build.ps1
+++ build.ps1
@@ -1,11 +1,7 @@
 $PROJECT_NAME = "ASFEnhance"
 $PLUGIN_NAME = "ASFEnhance.dll"
-$PROJECT_NAME2 = "ASFEnhance.IPC"
-$PLUGIN_NAME2 = "ASFEnhance.IPC.dll"
 
-dotnet publish $PROJECT_NAME -o ./publish/ -c Release
-dotnet publish $PROJECT_NAME2 -o ./publish/IPC/ -c Release
-
+dotnet publish $PROJECT_NAME -o ./publish/ -p:IpcEnable=true -c Release
 
 if (-Not (Test-Path -Path ./dist)) {
     New-Item -ItemType Directory -Path ./dist
@@ -14,13 +10,12 @@ else {
     Remove-Item -Path ./dist/* -Recurse -Force
 }
 
-Copy-Item -Path .\publish\$PLUGIN_NAME -Destination .\dist\ 
-Copy-Item -Path .\publish\IPC\$PLUGIN_NAME2 -Destination .\dist\ 
+Copy-Item -Path .\publish\$PLUGIN_NAME -Destination .\dist\
 
 $dirs = Get-ChildItem -Path ./publish -Directory
 foreach ($dir in $dirs) {
     $subFiles = Get-ChildItem -Path $dir.FullName -File -Filter *.resources.dll
-    
+
     foreach ($file in $subFiles) {
         $resourceName = [System.IO.Path]::GetFileNameWithoutExtension($file.Name)
         $opDir = "./dist/$resourceName"
